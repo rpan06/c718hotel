86d153e2d47abb99453eb0d788618493a8c5cf32
Jonathan Gallo <jonnygallo@gmail.com>
2018-08-17 16:04:28 -0700

    updated constructor in customer

--- customer.js
+++ customer.js
@@ -1,15 +1,15 @@
 
 
 class Customer{
-	constructor( name ){
-		this.name = name;
+	constructor( customerName ){
+		this.name = customerName;
 		this.room = null; //room we are currently
-		this.isHungry = false;
+		this.isHungry = false; // Always
 	}
 	orderRoomService( food ){
 		this.room.getRoomService(food);
 	}
 	eat( food ){
-		
 	}
+		
 }
