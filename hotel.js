e0d787c4341eb6d83f8cc1cbbe91c685025dd11f
Rachel Pan <rpan06@gmail.com>
2018-08-17 16:03:15 -0700

    added hireStaff to main.js and constructor items to staff.js -RP

--- hotel.js
+++ hotel.js
@@ -1,31 +1,34 @@
 
 
-class Hotel{
-	constructor(){
+class Hotel {
+	constructor() {
 		this.name = 'marriot';
 		this.rooms = [];
 		this.staff = [];
 	}
 	//make a customer object with the given name and add to a room.  need to add to a room with no occupants
-	checkInCustomer( name ){
+	checkInCustomer(name) {
 
 	}
-	checkOutCustomer(){
+	checkOutCustomer() {
 
 	}
 	//make a room and add to rooms array
-	addRoom(){
-
+	addRoom() {
+		var newRoom = new Room(this); //need to pass hotel object to room, so "this"
+		this.rooms.push(newRoom);
 	}
 	//add staff to the hotel
-	hireStaff( name ) {
+	hireStaff(name, currentHotel) {
+		var newStaff = new Staff('bobby', this);
+		this.staff.push(newStaff)
 
 	}
-	getVacantRooms(){
+	getVacantRooms() {
 
 	}
-	getKitchenService(){
-		
+	getKitchenService() {
+
 	}
 
 }
