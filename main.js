63e0ca58cf4c4e0593f84b25fb1b15f6d922c720
Rachel Pan <rpan06@gmail.com>
2018-08-17 17:06:41 -0700

    added 5 rooms to hotel in main.js -RP

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-//TODO: In main.js, add 5 rooms to your hotel
+
 
 var marriot = new Hotel();
 
-var room1 = new Room();
-var room2 = new Room();
-var room3 = new Room();
-var room4 = new Room();
-var room5 = new Room();
+for (var roomIndex = 0; roomIndex<5; roomIndex++){
+    marriot.addRoom();
+}
