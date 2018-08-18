4da2ccf1d72af9cf9e30934f176d283c67ff0db5
Mia Tran <maiptran157@gmail.com>
2018-08-17 18:00:17 -0700

    add code to removeOccupant function

--- room.js
+++ room.js
@@ -1,20 +1,22 @@
 
 
-class Room{
-	constructor(currentHotel){
+class Room {
+	constructor(currentHotel) {
 		this.occupant = null; //a customer
 		this.hotel = currentHotel;
 	}
 	addOccupant(customer) {
 		this.occupant = customer;
+		customer.assignRoom(this);
 	}
 	checkIfOccupied() {
 		//return if occupied, false if not
 	}
 	removeOccupant() {
-
+		customer.checkOutOfRoom(this);
+		this.occupant = null;
 	}
 	getRoomService(food) {
-		this.hotel.getKitchenService();
+		this.hotel.getKitchenService(food,this); //food from this room
 	}
 }
