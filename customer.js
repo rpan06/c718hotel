369f3505f8fe5ed8c7768d035face0a9ae89bd3e
Jonathan Gallo <jonnygallo@gmail.com>
2018-08-17 18:20:09 -0700

    hotel exercise updates made to customer.js

--- customer.js
+++ customer.js
@@ -5,11 +5,28 @@ class Customer{
 		this.name = customerName;
 		this.room = null; //room we are currently
 		this.isHungry = false;
+		this.ThingsIveEaten = [];
+		this.AmIHungry = setInterval(this.checkIfHungry.bind(this), 10000);
 	}
-	orderRoomService( food ){
+	assignRoom (room) {
+		this.room = room;
+	}
+	orderRoomService ( food ){
 		this.room.getRoomService(food);
 	}
 	eat( food ){
+		this.ThingsIveEaten.push(food)
+		this.isHungry = false;
 	}
-		
+	checkIfHungry () {
+		var misoHungry = Math.floor(Math.random() * 20) + 1 
+		if (misoHungry === 1) {
+			this.isHungry = true;
+			this.orderRoomService("creme brulee");
+		}
+	}
+	checkOutOfRoom (room) {
+		this.room = room;
+	}
+
 }
