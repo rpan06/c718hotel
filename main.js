ff49a33ec1b0a207f11c0052a2edfd9bb628830e
Rachel Pan <rpan06@gmail.com>
2018-08-17 17:10:04 -0700

    added 4 occupants to hotel -RP

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ var marriot = new Hotel();
 for (var roomIndex = 0; roomIndex<5; roomIndex++){
     marriot.addRoom();
 }
+
+var nameArray = ["Gerry", "Jon", "Rachel", "Mia"]
+for (var nameIndex = 0; nameIndex<4; nameIndex++){
+    marriot.checkInCustomer(nameArray[nameIndex]);
+}
