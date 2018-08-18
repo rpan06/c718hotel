25e44566bc31ed9accc215e90d74aedb46f32f9a
Rachel Pan <rpan06@gmail.com>
2018-08-17 17:27:08 -0700

    added kitchen request functions in staff.js -RP

--- staff.js
+++ staff.js
@@ -6,7 +6,14 @@ class Staff{
 		this.hotel = currentHotel;
 	}
 	handleKitchenRequest(){
-
+		this.busy = true;
+		var cookingFoodTimer = Math.floor(Math.random() * 30000) //30000 is 30 seconds
+		setTimeout(this.completeKitchenRequest, cookingFoodTimer)
+	}
+	completeKitchenRequest(){
+		this.isHungry = false;
+		this.busy = true;
+		console.log("Creme Brulee is delivered to the customer!")
 	}
 
 }
